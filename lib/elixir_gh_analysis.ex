c7a151a990346435dfcc9fe38369f37a9ab80838
Rachael Serur <rachaelse@gmail.com>
2017-06-17 14:55:27 -0400

    Initial commit

---
+++ lib/elixir_gh_analysis.ex
@@ -0,0 +1,18 @@
+defmodule ElixirGHAnalysis do
+  @moduledoc """
+  Documentation for ElixirGHAnalysis.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ElixirGHAnalysis.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/elixir_gh_analysis_test.exs
@@ -0,0 +1,8 @@
+defmodule ElixirGHAnalysisTest do
+  use ExUnit.Case
+  doctest ElixirGHAnalysis
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
